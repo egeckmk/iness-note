db898be242ad0a926c633a9c90094a49411fb667
egeckmk <egeckmk@gmail.com>
2022-03-28 18:14:06 +0300

    Added Login and Register API

---
+++ backend/controllers/userControllers.js
@@ -0,0 +1,61 @@
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModels");
+const generateToken = require("../utils/generateToken");
+
+//@description     Auth the user
+//@route           POST /api/users/login
+//@access          Public
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      pic: user.pic,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password!");
+  }
+});
+
+//@description     Register new user
+//@route           POST /api/users/
+//@access          Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password, pic } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(404);
+    throw new Error("User already exists.");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    pic,
+  });
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      password: user.password,
+      isAdmin: user.isAdmin,
+      pic: user.pic,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("User not found!");
+  }
+});
+
+module.exports = { registerUser, authUser };
